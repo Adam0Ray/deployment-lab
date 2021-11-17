4c399986e7a6909879504553d9f1a498db69fe05
AdamRayPublic <aray58@gmail.com>
2021-11-17 13:52:30 -0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express'); //look inside node modules and give me access to express code
+
+const path = require('path');
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})//res.sendFile is expecting absolute file path to be sending(meaning not related to where we are currently at...it wants user/desktop/folder/file.html)
+// dirname represents index.js, ../index.html is telling path.join where index.html is
+
+//server ready to send file to anything that makes a request to it
+
+
+
+
+
+const port = process.env.PORT || 4006  //logical or statement to set port
+
+app.listen(port, ()=> {
+    console.log(`My app is JAMMIN on port number ${port}`)
+})  //will allow app to listen to port
